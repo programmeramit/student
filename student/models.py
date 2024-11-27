347f14d2c8c356a23a14ac0b42d51b8e435fa73b
programmeramit <kamit896837@outlook.com>
2024-11-27 16:34:45 +0530

    addedrewuirement.txt

--- student/models.py
+++ student/models.py
@@ -1,3 +1,10 @@
+# models.py
 from django.db import models
 
-# Create your models here.
+class FileUpload(models.Model):
+    title = models.CharField(max_length=200)
+    file = models.FileField(upload_to='uploads/')  # Specify the upload folder inside 'media'
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- student/views.py
+++ student/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,"index.html")
+
+def login(request):
+    return render(request,"login.html")
